e82a83e21481b0abdd6e14628bd183574595472f
xdhcvr <xdhcvr@gmail.com>
2019-03-24 01:58:25 +0700

    Удален BaseService

--- src/main/java/cvr/otus/service/BookService.java
+++ src/main/java/cvr/otus/service/BookService.java
@@ -4,7 +4,16 @@ import cvr.otus.domain.Author;
 import cvr.otus.domain.Book;
 import cvr.otus.domain.Genre;
 
-public interface BookService extends BaseService<Book> {
+import java.util.List;
+
+public interface BookService{
+    Book add(String name);
+
+    List<Book> getAll();
+
+    Book get(int id);
+
+
     Book addAuthor(Book book, Author author);
 
     Book addGenre(Book book, Genre genre);
--- src/main/java/cvr/otus/service/GenreService.java
+++ src/main/java/cvr/otus/service/GenreService.java
@@ -2,5 +2,13 @@ package cvr.otus.service;
 
 import cvr.otus.domain.Genre;
 
-public interface GenreService extends BaseService<Genre> {
+import java.util.List;
+
+public interface GenreService {
+    Genre add(String name);
+
+    List<Genre> getAll();
+
+    Genre get(int id);
+
 }
