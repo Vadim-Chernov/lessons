51fd7b9f7ccad8bf8c2e9b376be3bf21ccedd781
xdhcvr <xdhcvr@gmail.com>
2019-03-24 02:07:07 +0700

    Тесты воостановлены

--- src/test/java/cvr/otus/repo/AuthorRepositoryImplTest.java
+++ src/test/java/cvr/otus/repo/AuthorRepositoryImplTest.java
@@ -1,48 +1,48 @@
-//package cvr.otus.repo;
-//
-//
-//import cvr.otus.domain.Author;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertNotNull;
-//
-//@DisplayName("AuthorRepositoryImplTest интерграционный тест")
-//@RunWith(SpringJUnit4ClassRunner.class)// SpringRunner.class
-//@SpringBootTest
-//public class AuthorRepositoryImplTest {
-//
-//    @Autowired
-//    private AuthorRepository authorRepository;
-//
-//    @org.junit.Test
-//    @DisplayName("getById")
-//    public void getById() {
-//        Author author = authorRepository.add("Петров");
-//        Author byId = authorRepository.getById(author.getId());
-//        assertEquals(author.getId(),byId.getId());
-//    }
-//
-//    @org.junit.Test
-//    @DisplayName("add")
-//    public void add() {
-//        Author author = authorRepository.add("Иванов");
-//        int id = author.getId();
-//        Author byId = authorRepository.getById(id);
-//        assertEquals("Иванов",byId.getName());
-//
-//    }
-//
-//    @org.junit.Test
-//    @DisplayName("add")
-//    public void getAll() {
-//        List<Author> all = authorRepository.getAll();
-//        assertNotNull(all);
-//    }
-//}
+package cvr.otus.repo;
+
+
+import cvr.otus.domain.Author;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@DisplayName("AuthorRepositoryImplTest интерграционный тест")
+@RunWith(SpringJUnit4ClassRunner.class)// SpringRunner.class
+@SpringBootTest
+public class AuthorRepositoryImplTest {
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    @org.junit.Test
+    @DisplayName("getById")
+    public void getById() {
+        Author author = authorRepository.add("Петров");
+        Author byId = authorRepository.getById(author.getId());
+        assertEquals(author.getId(),byId.getId());
+    }
+
+    @org.junit.Test
+    @DisplayName("add")
+    public void add() {
+        Author author = authorRepository.add("Иванов");
+        int id = author.getId();
+        Author byId = authorRepository.getById(id);
+        assertEquals("Иванов",byId.getName());
+
+    }
+
+    @org.junit.Test
+    @DisplayName("add")
+    public void getAll() {
+        List<Author> all = authorRepository.getAll();
+        assertNotNull(all);
+    }
+}
--- src/test/java/cvr/otus/repo/BookRepositoryImplTest.java
+++ src/test/java/cvr/otus/repo/BookRepositoryImplTest.java
@@ -1,91 +1,91 @@
-//package cvr.otus.repo;
-//
-//import cvr.otus.domain.Author;
-//import cvr.otus.domain.Book;
-//import cvr.otus.domain.Genre;
-//import org.junit.Test;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertNotNull;
-//
-////import org.junit.jupiter.api.Test;
-//
-//@DisplayName("BookRepositoryImplTest интерграционный тест")
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@SpringBootTest
-//
-//public class BookRepositoryImplTest {
-//
-//    @Autowired
-//    private BookRepository repository;
-//
-//    @Autowired
-//    private GenreRepository genreRepository;
-//
-//    @Autowired
-//    private AuthorRepository authorRepository;
-//
-//
-//    @Test
-//    public void getById() {
-//        Book book = repository.add("123");
-//        Book byId = repository.getById(book.getId());
-//        assertEquals(book.getId(), byId.getId());
-//    }
-//
-//    @Test
-//    public void add() {
-//        Book book = repository.add("123");
-//        Book byId = repository.getById(book.getId());
-//        assertEquals("123", byId.getName());
-//    }
-//
-//    @Test
-//    public void getAll() {
-//        List<Book> all = repository.getAll();
-//        assertNotNull(all);
-//    }
-//
-//    @Test
-//    public void addGenre() {
-//        Genre genre = genreRepository.add("genre");
-//        Book add = repository.add("book");
-//        Book book = repository.addGenre(add, genre);
-//        assertEquals("genre", book.getGenres().get(0).getName());
-//    }
-//
-//    @Test
-//    public void addAuthor() {
-//        Author author = authorRepository.add("author");
-//        Book book = repository.add("book");
-//        repository.addAuthor(book, author);
-//        assertEquals("author", book.getAuthors().get(0).getName());
-//    }
-//
-//    @Test
-//    public void removeGenre() {
-//        Genre genre = genreRepository.add("genre");
-//        Book book = repository.add("book");
-//        book = repository.addGenre(book, genre);
-//        int size = book.getGenres().size();
-//        book = repository.removeGenre(book, genre);
-//        assertEquals(size - 1, book.getGenres().size());
-//    }
-//
-//    @Test
-//    public void removeAuthor() {
-//        Author author = authorRepository.add("author");
-//        Book book = repository.add("book");
-//        book = repository.addAuthor(book,author);
-//        int size = book.getAuthors().size();
-//        book = repository.removeAuthor(book,author);
-//        assertEquals(size - 1, book.getAuthors().size());
-//    }
-//}
+package cvr.otus.repo;
+
+import cvr.otus.domain.Author;
+import cvr.otus.domain.Book;
+import cvr.otus.domain.Genre;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+//import org.junit.jupiter.api.Test;
+
+@DisplayName("BookRepositoryImplTest интерграционный тест")
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+
+public class BookRepositoryImplTest {
+
+    @Autowired
+    private BookRepository repository;
+
+    @Autowired
+    private GenreRepository genreRepository;
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+
+    @Test
+    public void getById() {
+        Book book = repository.add("123");
+        Book byId = repository.getById(book.getId());
+        assertEquals(book.getId(), byId.getId());
+    }
+
+    @Test
+    public void add() {
+        Book book = repository.add("123");
+        Book byId = repository.getById(book.getId());
+        assertEquals("123", byId.getName());
+    }
+
+    @Test
+    public void getAll() {
+        List<Book> all = repository.getAll();
+        assertNotNull(all);
+    }
+
+    @Test
+    public void addGenre() {
+        Genre genre = genreRepository.add("genre");
+        Book add = repository.add("book");
+        Book book = repository.addGenre(add, genre);
+        assertEquals("genre", book.getGenres().get(0).getName());
+    }
+
+    @Test
+    public void addAuthor() {
+        Author author = authorRepository.add("author");
+        Book book = repository.add("book");
+        repository.addAuthor(book, author);
+        assertEquals("author", book.getAuthors().get(0).getName());
+    }
+
+    @Test
+    public void removeGenre() {
+        Genre genre = genreRepository.add("genre");
+        Book book = repository.add("book");
+        book = repository.addGenre(book, genre);
+        int size = book.getGenres().size();
+        book = repository.removeGenre(book, genre);
+        assertEquals(size - 1, book.getGenres().size());
+    }
+
+    @Test
+    public void removeAuthor() {
+        Author author = authorRepository.add("author");
+        Book book = repository.add("book");
+        book = repository.addAuthor(book,author);
+        int size = book.getAuthors().size();
+        book = repository.removeAuthor(book,author);
+        assertEquals(size - 1, book.getAuthors().size());
+    }
+}
