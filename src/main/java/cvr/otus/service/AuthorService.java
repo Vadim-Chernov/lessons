b543934d9acba5d2f45cb11bad59f961c4a2d915
xdhcvr <xdhcvr@gmail.com>
2019-03-25 04:33:37 +0700

    учет замечаний. Удаление BaseService

--- src/main/java/cvr/otus/service/AuthorService.java
+++ src/main/java/cvr/otus/service/AuthorService.java
@@ -2,5 +2,13 @@ package cvr.otus.service;
 
 import cvr.otus.domain.Author;
 
-public interface AuthorService extends BaseService<Author> {
+import java.util.List;
+
+public interface AuthorService {
+    Author add(String name);
+
+    List<Author> getAll();
+
+    Author get(int id);
+
 }
--- src/main/java/cvr/otus/service/GenreService.java
+++ src/main/java/cvr/otus/service/GenreService.java
@@ -2,5 +2,12 @@ package cvr.otus.service;
 
 import cvr.otus.domain.Genre;
 
-public interface GenreService extends BaseService<Genre> {
+import java.util.List;
+
+public interface GenreService {
+    Genre add(String name);
+
+    List<Genre> getAll();
+
+    Genre get(int id);
 }
