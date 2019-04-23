a7e9fcf0c09bef15d74c16f646f3935d950ec127
chernov <chernov.vr@gmail.com>
2019-04-24 06:16:52 +0700

    Spring shell was removed

--- src/main/java/cvr/otus/config/Config.java
+++ src/main/java/cvr/otus/config/Config.java
@@ -1,43 +1,26 @@
 package cvr.otus.config;
 
-import liquibase.integration.spring.SpringLiquibase;
 import org.h2.tools.Console;
-import org.jline.utils.AttributedString;
-import org.jline.utils.AttributedStyle;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.shell.jline.PromptProvider;
 
 @Configuration
 public class Config {
-    /**
-     * Бин сделан "вражьим" способом)))
-     * чтоб запустить Н2-консоль
-     * @return Консоль Н2
-     */
-    @Bean
-    public Console console() {
-        Console con = null;
-        try {
-            Console.main();
-            con = Console.class.newInstance();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return con;
-    }
-
+//    /**
+//     * Бин сделан "вражьим" способом)))
+//     * чтоб запустить Н2-консоль
+//     * @return Консоль Н2
+//     */
 //    @Bean
-//    public SpringLiquibase liquibase() {
-//        SpringLiquibase liquibase = new SpringLiquibase();
-//        liquibase.setChangeLog("classpath:db.changelog-master.yaml");
-////        liquibase.setDataSource(dataSource());
-//        return liquibase;
+//    public Console console() {
+//        Console con = null;
+//        try {
+//            Console.main();
+//            con = Console.class.newInstance();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return con;
 //    }
 
-
-    @Bean
-    public PromptProvider promptProvider() {
-        return () -> new AttributedString("otus:>", AttributedStyle.DEFAULT.foreground(AttributedStyle.YELLOW));
-    }
 }
