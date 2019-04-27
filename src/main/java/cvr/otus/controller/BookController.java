2b89c24ebdc2d985e442831fd3c20262d1abcc71
chernov <chernov.vr@gmail.com>
2019-04-27 11:32:47 +0700

    ДЗ №8 к уроку №12(с ошибками)

--- src/main/java/cvr/otus/controller/BookController.java
+++ src/main/java/cvr/otus/controller/BookController.java
@@ -19,20 +19,20 @@ public class BookController {
     @Autowired
     public BookController(BookService bookService) {
         this.bookService = bookService;
-        test();
+//        test();
     }
 
-    private void test() {
-        List<Book> books = bookService.findAll();
-        Book book = books.get(0);
-        System.out.println("***** notAuthors = " + bookService.notAuthors(book.getId()) +" authors = " +book.getAuthors());
-        System.out.println("***** notGenres  = " + bookService.notGenres(book.getId())  +" genres  = " +book.getGenres());
-
-    }
+//    private void test() {
+//        List<Book> books = bookService.findAll();
+//        Book book = books.get(0);
+//        System.out.println("***** notAuthors = " + bookService.notAuthors(book.getId()) +" authors = " +book.getAuthors());
+//        System.out.println("***** notGenres  = " + bookService.notGenres(book.getId())  +" genres  = " +book.getGenres());
+//
+//    }
 
     @RequestMapping(value = "/")
     public String bookList(Model model) {
-        test();
+//        test();
 
         List<Book> all = bookService.findAll();
         Book book = all.get(0);
@@ -43,7 +43,7 @@ public class BookController {
 
     @RequestMapping(value = {"/bookEdit", "/bookEdit/{id}"}, method = RequestMethod.GET)
     public String bookEdit(Model model, @PathVariable(required = false, name = "id") String id) {
-        test();
+//        test();
         if (null != id) {
             Book book = bookService.get(id);
             model.addAttribute("book", book);
@@ -56,7 +56,7 @@ public class BookController {
 
     @RequestMapping(value = "/bookEdit", method = RequestMethod.POST)
     public String bookEdit(Model model, Book book) {
-        test();
+//        test();
         Book book1 = bookService.get(book.getId());
         if (book1 != null) {
             book1.setName(book.getName());
@@ -72,7 +72,7 @@ public class BookController {
 
     @RequestMapping(value = "/bookDelete/{id}", method = RequestMethod.GET)
     public String bookDeleted(Model model, @PathVariable(required = true, name = "id") String id) {
-        test();
+//        test();
         bookService.remove(id);
         model.addAttribute("bookList", bookService.findAll());
         return "bookList";
@@ -82,7 +82,7 @@ public class BookController {
     public String authorDelete(Model model,
                                @PathVariable(required = true, name = "book_id") String book_id,
                                @PathVariable(required = true, name = "author_id") String author_id) {
-        test();
+//        test();
         bookService.removeAuthor(book_id,author_id);
         Book book = bookService.get(book_id);
         model.addAttribute("book", book);
@@ -92,7 +92,7 @@ public class BookController {
     public String authorAdd(Model model,
                                @PathVariable(required = true, name = "book_id") String book_id,
                                @PathVariable(required = true, name = "author_id") String author_id) {
-        test();
+//        test();
         bookService.addAuthor(book_id,author_id);
         Book book = bookService.get(book_id);
         model.addAttribute("book", book);
@@ -104,7 +104,7 @@ public class BookController {
     public String genreDelete(Model model,
                                @PathVariable(required = true, name = "book_id") String book_id,
                                @PathVariable(required = true, name = "genre_id") String genre_id) {
-        test();
+//        test();
         bookService.removeGenre(book_id,genre_id);
         Book book = bookService.get(book_id);
         model.addAttribute("book", book);
@@ -115,21 +115,10 @@ public class BookController {
     public String genreAdd(Model model,
                               @PathVariable(required = true, name = "book_id") String book_id,
                               @PathVariable(required = true, name = "genre_id") String genre_id) {
-        test();
+//        test();
         bookService.addGenre(book_id,genre_id);
         Book book = bookService.get(book_id);
         model.addAttribute("book", book);
         return "bookEdit";
     }
-
-//
-//    @RequestMapping(value = "/addAuthor/{id}", method = RequestMethod.GET)
-//    public String addAuthor(Model model, @PathVariable(required = true, name = "id") Long id) {
-//        if (null != id) {
-//            model.addAttribute("book", bookService.get(id));
-//        } else {
-//            model.addAttribute("book", new Book());
-//        }
-//        return "addAuthor-УДАЛИТЬ";
-//    }
 }
